06b2b82075e2bbb5d0bc6fec5dc83edb43717189
anzhelapetrosyan <81684595+anzhelapetrosyan@users.noreply.github.com>
2021-04-09 14:22:29 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require('mongodb').ObjectId; 
 
 const uri =
- mongodb+srv://anzhela_admin:Anjela099@cluster0.fj7un.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+ "mongodb+srv://anzhela_admin:Anjela099@cluster0.fj7un.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
