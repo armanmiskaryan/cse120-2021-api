7edc6f12da5100de9f6329bad9e44b50a5a0f7fd
anzhelapetrosyan <81684595+anzhelapetrosyan@users.noreply.github.com>
2021-04-14 17:37:46 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -227,7 +227,7 @@ function displayData(data) {
 }
 var loadedData = [];
 
-function loadBookEditItem() {
+function loadEditItem() {
     localStorage = window.localStorage;
     editItem = JSON.parse(localStorage.getItem("editItem"));
     console.log(editItem);
